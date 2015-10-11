e1538d66cc489f7ab105753dbf486063a1ec47a3
Jay Hannah <jay.hannah@iinteractive.com>
2015-10-11 11:16:07 -0500

    Real data pumping into the charts

--- static-www/scripts/generate_www.pl
+++ static-www/scripts/generate_www.pl
@@ -2,35 +2,41 @@ use 5.22.0;
 use DBI;
 use Template;
 
-use DBI;
-my $dbh = DBI->connect("dbi:SQLite:dbname=../db/db.sqlite3");
-my $strsql = <<EOT;
-select tax_year, sum(total_tif_base_taxes), sum(total_tif_excess_taxes)
-from year
-group by tax_year
-EOT
-my $sth = $dbh->prepare($strsql);
-$sth->execute;
-while (my @row = $sth->fetchrow) {
-  say join " ", @row;
-}
-
 my $tt = Template->new({
   INCLUDE_PATH => 'templates',
   INTERPOLATE  => 1,
 }) || die "$Template::ERROR\n";
 
-# https://google-developers.appspot.com/chart/interactive/docs/gallery/barchart
-my $data = <<EOT;
-          ['2010', 10, 24, ''],
-          ['2021', 16, 22, ''],
-          ['2030', 28, 19, '']
-EOT
-
 my $vars = {
-  data => $data,
+  data => fetch_chart_data(),
 };
 
 $tt->process('chart.tt2', $vars, 'www/index.html')
   || die $tt->error(), "\n";
+
+
+# end main
+
+
+sub fetch_chart_data {
+  my $dbh = DBI->connect("dbi:SQLite:dbname=../db/db.sqlite3");
+  my $strsql = <<EOT;
+select tax_year, sum(total_tif_base_taxes), sum(total_tif_excess_taxes)
+from year
+group by tax_year
+EOT
+  my $sth = $dbh->prepare($strsql);
+  $sth->execute;
+
+  my @js_data;
+  while (my @row = $sth->fetchrow) {
+    say join " ", @row;
+    # Our series are each a string in javascript syntax like this:
+    # ['2010', 10, 24, ''],
+    push @js_data, sprintf("['%s', %s, %s, '']", @row);
+  }
+  # Squish the series together with another comma
+  return join ",", @js_data;
+}
+
 
