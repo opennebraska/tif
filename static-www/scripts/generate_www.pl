bcb4a7da045d218bba926b4bf9527a607190487c
Jay Hannah <jay.hannah@iinteractive.com>
2015-10-11 10:40:20 -0500

    Direct DB access for now

--- static-www/scripts/generate_www.pl
+++ static-www/scripts/generate_www.pl
@@ -1,4 +1,19 @@
+use 5.22.0;
+use DBI;
 use Template;
+
+use DBI;
+my $dbh = DBI->connect("dbi:SQLite:dbname=../db/db.sqlite3");
+my $strsql = <<EOT;
+select tax_year, sum(total_tif_base_taxes), sum(total_tif_excess_taxes)
+from year
+group by tax_year
+EOT
+my $sth = $dbh->prepare($strsql);
+$sth->execute;
+while (my @row = $sth->fetchrow) {
+  say join " ", @row;
+}
 
 my $tt = Template->new({
   INCLUDE_PATH => 'templates',
@@ -6,12 +21,11 @@ my $tt = Template->new({
 }) || die "$Template::ERROR\n";
 
 my $vars = {
-    name     => 'Count Edward van Halen',
-    debt     => '3 riffs and a solo',
-    deadline => 'the next chorus',
+  name     => 'Count Edward van Halen',
+  debt     => '3 riffs and a solo',
+  deadline => 'the next chorus',
 };
 
 $tt->process('chart.tt2', $vars, 'www/index.html')
-    || die $tt->error(), "\n";
-close $out;
+  || die $tt->error(), "\n";
 
