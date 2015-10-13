849303843a6b438597828e5db9aafa96eb7fabd9
Jay Hannah <jay.hannah@iinteractive.com>
2015-10-12 22:01:49 -0500

    Generate a page per county

--- static-www/scripts/generate_www.pl
+++ static-www/scripts/generate_www.pl
@@ -10,10 +10,36 @@ my $tt = Template->new({
 }) || die "$Template::ERROR\n";
 
 generate_homepage();
-generate_county_list();
+generate_county_pages();
 
 
 # end main
+
+sub generate_county_pages {
+  my $strsql = <<EOT;
+select distinct county_name from project order by 1
+EOT
+  my $sth = $dbh->prepare($strsql);
+  $sth->execute;
+  my @counties;
+  while (my ($name) = $sth->fetchrow) {
+    my $directory_name = $name;
+    $directory_name =~ s/ /_/g;
+    my $pretty_name = $name;
+    $pretty_name = join ' ', map({ ucfirst() } split / /, lc $name);
+    push @counties, "<a href='$directory_name/'>$pretty_name</a>";
+
+    my $vars = {
+      chart_data => fetch_chart_data("and county_name = '$name'"),
+      #children   => $counties,
+      title      => "$pretty_name County TIF Report 2014",
+    };
+    my $outfile = "www/$directory_name/index.html";
+    say "Generting $outfile";
+    $tt->process('index.tt2', $vars, $outfile) || die $tt->error(), "\n";
+  }
+  return join ", \n", @counties;
+}
 
 sub generate_county_list {
   my $strsql = <<EOT;
@@ -35,16 +61,22 @@ sub generate_homepage {
   my $counties = generate_county_list();
   my $vars = {
     chart_data => fetch_chart_data(),
-    counties   => $counties,
+    children   => $counties,
+    title      => "Nebraska TIF Report 2014",
   };
-  $tt->process('index.tt2', $vars, 'www/index.html')
-    || die $tt->error(), "\n";
+
+  my $outfile = "www/index.html";
+  say "Generting $outfile";
+  $tt->process('index.tt2', $vars, $outfile) || die $tt->error(), "\n";
 }
 
 sub fetch_chart_data {
+  my ($more_where) = @_;
   my $strsql = <<EOT;
 select tax_year, sum(total_tif_base_taxes), sum(total_tif_excess_taxes)
-from year
+from project p, year y
+where p.tif_id = y.tif_id
+$more_where
 group by tax_year
 EOT
   my $sth = $dbh->prepare($strsql);
@@ -52,7 +84,7 @@ EOT
 
   my @js_data;
   while (my @row = $sth->fetchrow) {
-    say join " ", @row;
+    # say join " ", @row;
     # Our series are each a string in javascript syntax like this:
     # ['2010', 10, 24, ''],
     push @js_data, sprintf("['%s', %s, %s, '']", @row);
