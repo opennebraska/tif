ab970bfc6b6aaf4193f6172d13dd4224230a0af4
Jay Hannah <jay.hannah@iinteractive.com>
2015-10-12 21:46:06 -0500

    County list

--- static-www/scripts/generate_www.pl
+++ static-www/scripts/generate_www.pl
@@ -2,24 +2,46 @@ use 5.22.0;
 use DBI;
 use Template;
 
+my $dbh = DBI->connect("dbi:SQLite:dbname=../db/db.sqlite3");
+
 my $tt = Template->new({
   INCLUDE_PATH => 'templates',
   INTERPOLATE  => 1,
 }) || die "$Template::ERROR\n";
 
-# Generate homepage:
-my $vars = {
-  data => fetch_chart_data(),
-};
-$tt->process('chart.tt2', $vars, 'www/index.html')
-  || die $tt->error(), "\n";
+generate_homepage();
+generate_county_list();
 
 
 # end main
 
+sub generate_county_list {
+  my $strsql = <<EOT;
+select distinct county_name from project order by 1
+EOT
+  my $sth = $dbh->prepare($strsql);
+  $sth->execute;
+  my @counties;
+  while (my ($name) = $sth->fetchrow) {
+    my $directory_name = $name;
+    $directory_name =~ s/ /_/g;
+    $name = join ' ', map({ ucfirst() } split / /, lc $name);
+    push @counties, "<a href='$directory_name/'>$name</a>";
+  }
+  return join ", \n", @counties;
+}
+
+sub generate_homepage {
+  my $counties = generate_county_list();
+  my $vars = {
+    chart_data => fetch_chart_data(),
+    counties   => $counties,
+  };
+  $tt->process('index.tt2', $vars, 'www/index.html')
+    || die $tt->error(), "\n";
+}
 
 sub fetch_chart_data {
-  my $dbh = DBI->connect("dbi:SQLite:dbname=../db/db.sqlite3");
   my $strsql = <<EOT;
 select tax_year, sum(total_tif_base_taxes), sum(total_tif_excess_taxes)
 from year
