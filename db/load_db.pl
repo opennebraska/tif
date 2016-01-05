2448d68f95b73f55b945fb53882072bf78575c56
Jay Hannah <jay.hannah@iinteractive.com>
2016-01-04 20:06:27 -0600

    Woot! Multi-file is working! :)

--- db/load_db.pl
+++ db/load_db.pl
@@ -4,10 +4,14 @@ use 5.22.0;
 use Text::CSV_XS;
 use Data::Printer;
 use TIF;
-
-usage() unless (-r $ARGV[0]);
+use FileHandle;
+STDOUT->autoflush();
+STDERR->autoflush();
 
 my $schema = TIF->connect('dbi:SQLite:dbname=db.sqlite3');
+# Nuke all existing data:
+$schema->resultset('Project')->delete;
+$schema->resultset('Year')->delete;
 
 my %column_names = (
   0  => "tif_id",
@@ -38,88 +42,78 @@ my %column_names = (
   25 => "total_tif_base_taxes",
 );
 
-my $tif_id = {};
 my $csv = Text::CSV_XS->new ({ binary => 1, auto_diag => 1 });
-open my $fh, "<:encoding(utf8)", $ARGV[0] or die $!;
-$csv->getline($fh);     # Discard headers
 my $project;
-while (my $row = $csv->getline ($fh)) {
-  my $id = $row->[0];
-  my ($name, $location, $description) = 
-    map { s#.*?: ##; $_ }                # discard "Prefix: " strings
-    ( split /[\r\n]+/s, $row->[10] );
-  $tif_id->{$id} = {
-    name        => $name,
-    location    => $location,
-    description => $description,
-  };
+my $tif_id = {};
 
-  unless ($project && $project->id eq $id) {
-    $project = $schema->resultset('Project')->find($id);
-  }
-  unless ($project) {
-    # Create this project
-    my %db_row = ();
-    foreach my $col (0..9) {
-      $db_row{$column_names{$col}} = $row->[$col];
+my @files = glob("*.csv");
+foreach my $file (@files) {
+  say "\n\n$file...";
+  process_file($file);
+}
+
+# end main
+
+
+sub process_file { 
+  my ($file) = @_;
+  open my $fh, "<:encoding(utf8)", $file or die $!;
+
+  while (my $row = $csv->getline ($fh)) {
+    my $id = $row->[0];
+    next unless ($id =~ /\d\d\-\d\d\d\d/);  # Skip headers
+    my ($name, $location, $description) = 
+      map { s#.*?: ##; $_ }                # discard "Prefix: " strings
+      ( split /[\r\n]+/s, $row->[10] );
+    $tif_id->{$id} = {
+      name        => $name,
+      location    => $location,
+      description => $description,
     };
-    $db_row{name}        = $name;
-    $db_row{location}    = $location;
-    $db_row{description} = $description;
-
-    my $p = $schema->resultset('Project')->new(\%db_row)->insert;
-    print "\n" . $p->id . " ";
-  }
-
-  my $tax_year = $row->[11];
-  my $year = $schema->resultset('Year')->find($id, $tax_year);
-  my %db_row = (
-    tif_id   => $id,
-    tax_year => $tax_year,
-  );
-  foreach my $col (12..25) {
-    my $val = $row->[$col];
-    $val =~ s/,//g;
-    $db_row{$column_names{$col}} = $val;
-  };
-  if ($year) {
-    # Audit existing data
-    $year->set_columns(\%db_row);
-    if (my %dc = $year->get_dirty_columns) {
-      p %dc;
-      die "Aaaaa!! $id $tax_year";
-    } else {
-      say "\n$tax_year audit was clean";
+  
+    unless ($project && $project->id eq $id) {
+      print "\n$id ";
+      $project = $schema->resultset('Project')->find($id);
     }
-  } else {
-    # Create new row in year table
-    my $y = $schema->resultset('Year')->new(\%db_row)->insert;
-    print $y->tax_year . " ";
+    unless ($project) {
+      # Create this project
+      my %db_row = ();
+      foreach my $col (0..9) {
+        $db_row{$column_names{$col}} = $row->[$col];
+      };
+      $db_row{name}        = $name;
+      $db_row{location}    = $location;
+      $db_row{description} = $description;
+  
+      $project = $schema->resultset('Project')->new(\%db_row)->insert;
+    }
+  
+    my $tax_year = $row->[11];
+    my $year = $schema->resultset('Year')->find($id, $tax_year);
+    my %db_row = (
+      tif_id   => $id,
+      tax_year => $tax_year,
+    );
+    foreach my $col (12..25) {
+      my $val = $row->[$col];
+      $val =~ s/,//g;
+      $db_row{$column_names{$col}} = $val;
+    };
+    if ($year) {
+      # Audit existing data
+      $year->set_columns(\%db_row);
+      if (my %dc = $year->get_dirty_columns) {
+        print "\n";
+        p %dc;
+        say "PANIC on $file $id $tax_year";
+        next;
+      }
+    } else {
+      # Create new row in year table
+      $year = $schema->resultset('Year')->new(\%db_row)->insert;
+    }  
+    print $year->tax_year . " ";
   }
 }
-close $fh;
-
-sub usage {
-  print <<EOT;
-$0 TIF_Report_2014.csv
-
-Load that .csv file into the SQLite database.
-
-EOT
-  exit 1;
-}
-
-
-__END__
-
-# Somebody asked for some CSV output. Generate that as 'new.csv':
-$csv->eol("\n");
-open my $fh, ">:encoding(utf8)", "new.csv" or die "new.csv: $!";
-foreach my $id (keys %$tif_id) {
-  my $data = $tif_id->{$id};
-  say join "|", $id, $data->{name}, $data->{location}, $data->{description};
-  $csv->print($fh, [$id, $data->{name}, $data->{location}, $data->{description}]);
-}
-close $fh or die "new.csv: $!";
 
 
