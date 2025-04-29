eb89bcf6411b13cc5f6babe7d03671464702bc11
Sefa Bulak <zckhtln@icloud.com>
2025-04-29 21:52:47 +0300

    Add search functionality and index generation for counties, cities, and TIF projects

--- static-www/scripts/generate_www.pl
+++ static-www/scripts/generate_www.pl
@@ -5,6 +5,7 @@ use DBI;
 use Template;
 use File::Path qw(make_path);
 use Memoize;
+use JSON;
 
 =head1 NAME
 
@@ -34,6 +35,7 @@ generate_homepage();
 generate_county_pages();
 generate_city_pages();
 generate_tif_pages();
+generate_search_index();
 
 # end main
 
@@ -202,6 +204,58 @@ EOT
     say "Generating $outfile";
     $tt->process('index.tt2', $vars, $outfile) || die $tt->error(), "\n";
   }
+}
+
+sub generate_search_index {
+  my @search_data;
+
+  my $county_sql = "select distinct county_name from project order by 1";
+  my $county_sth = $dbh->prepare($county_sql);
+  $county_sth->execute or die "Error (counties): " . $dbh->errstr;
+  while (my ($name) = $county_sth->fetchrow) {
+    my ($directory_name, $pretty_name) = names($name);
+    push @search_data, {
+      id   => "county_$directory_name",
+      name => $pretty_name,
+      type => "County",
+      url  => "/$directory_name/index.html",
+    };
+  }
+
+  my $city_sql = "select distinct county_name, city_name from project order by 1";
+  my $city_sth = $dbh->prepare($city_sql);
+  $city_sth->execute or die "Error (cities): " . $dbh->errstr;
+  while (my ($county, $city) = $city_sth->fetchrow) {
+    my ($co_directory, $co_pretty) = names($county);
+    my ($ci_directory, $ci_pretty) = names($city);
+    push @search_data, {
+      id   => "city_${co_directory}_${ci_directory}",
+      name => $ci_pretty,
+      type => "City",
+      url  => "/$co_directory/$ci_directory/index.html",
+    };
+  }
+
+  my $tif_sql = "select county_name, city_name, tif_id, name from project";
+  my $tif_sth = $dbh->prepare($tif_sql);
+  $tif_sth->execute or die "Error (TIF projects): " . $dbh->errstr;
+  while (my $row = $tif_sth->fetchrow_hashref) {
+    my ($co_directory, $co_pretty) = names($row->{county_name});
+    my ($ci_directory, $ci_pretty) = names($row->{city_name});
+    push @search_data, {
+      id   => "tif_$row->{tif_id}",
+      name => $row->{name},
+      type => "TIF Project",
+      url  => "/$co_directory/$ci_directory/$row->{tif_id}.html",
+    };
+  }
+
+  my $json = JSON->new->pretty->encode(\@search_data);
+  my $outfile = "$out_root/search-index.json";
+  open my $fh, '>', $outfile or die "error: $!";
+  print $fh $json;
+  close $fh;
+  say "Generated search index: $outfile";
 }
 
 sub names {
