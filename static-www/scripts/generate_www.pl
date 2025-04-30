cadc5e2141bc3d21910f8b07c0e702b4de584f99
Sefa Bulak <zckhtln@icloud.com>
2025-04-30 10:28:36 +0300

    Refactor code structure for improved readability and maintainability

--- static-www/scripts/generate_www.pl
+++ static-www/scripts/generate_www.pl
@@ -58,7 +58,7 @@ sub generate_homepage {
     chart_data => fetch_chart_data(),
     this_total => $this_total,
     children   => $county_list,
-    title      => "Nebraska TIF Report 2024",
+    title      => "2024 TIF Report",
     url        => $url_root,
   };
   my $outfile = "$out_root/index.html";
